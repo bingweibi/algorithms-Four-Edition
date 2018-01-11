d01fa1d34019315eb0a1b9f7d03c728404d76988
bibingwei <794654932@>
2018-01-11 10:55:19 +0800

    0111算法

---
+++ chapter1/src/excerise/excerise1.java
@@ -0,0 +1,17 @@
+package excerise;
+
+/**
+ * 1.1的练习
+ * @author bibingwei
+ */
+public class excerise1 {
+    public static void main(String[] args) {
+        System.out.println("1.1.1: " + (15) /2);
+        System.out.println("1.1.1: " + (2.0e-6 * 100000000.1));
+        System.out.println("1.1.1: " + true);
+        System.out.println("1.1.2: " + (1 + 2.236)/2);
+        System.out.println("1.1.2: " + (1+2+3+4.0));
+        System.out.println("1.1.2: " + (4.1 >= 4));
+        System.out.println("1.1.2: " + (1 + 2 + "3"));
+    }
+}
